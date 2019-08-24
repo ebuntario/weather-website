72fe0bca9b8f14641ddcc298b48dbc954cb4b2de
Regnauld Ethan Fane Buntario <regnauld.buntario@traveloka.com>
2019-08-24 16:57:33 +0530

    Adding daily summary forecast.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
     } else {
       callback(
         undefined,
-        `It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.`
+        `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.`
       );
     }
   });
